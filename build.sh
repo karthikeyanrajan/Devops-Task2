95db0ac89bab863faf1dd76cf106b81bb7997b7d
karthikeyanrajan <138673317+karthikeyanrajan@users.noreply.github.com>
2024-01-16 22:27:17 +0530

    Update build.sh

--- build.sh
+++ build.sh
@@ -7,7 +7,7 @@ IMAGE_TAG="latest"
 # Build the Docker image
 echo -e "\e[32m Docker image build is started \e[0m"
 echo " "
-docker compose build  
+docker composee build  
 
 # Check if the build was successful
 if [ $? -eq 0 ]; then
